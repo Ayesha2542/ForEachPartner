77ef5a2c4338cce278f4f6a23b1581c37f0aad04
ToqeerFatima <125538720+ToqeerFatima@users.noreply.github.com>
2023-09-16 03:43:43 -0700

    fffg
    mklk;

--- src/screens/Orders/Orders.js
+++ src/screens/Orders/Orders.js
@@ -7,64 +7,58 @@ import AppColors from '../../assets/colors/AppColors';
 import ContainerStyles from '../../assets/Styles/ContainerStyles';
 import { widthPercentageToDP as wp,heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import TextStyles from '../../assets/Styles/TextStyles';
+import ImageStyles from '../../assets/Styles/ImageStyles'
+
 const Orders = ({navigation}) => {
   return (
     <SafeAreaView style={styles.container}>
             <ProfileHeader navigation={navigation} item="Orders" /> 
-            <View style={{ alignItems: 'center' }}>
+
+
+<View style={{alignItems:"center",flexDirection:"row",justifyContent:"space-evenly"}}>
             <TouchableOpacity onPress={()=>{
               navigation.navigate('OngoingOrder')
             }}>
           <Neomorph
-            // darkShadowColor={AppColors.primary}
-            // lightShadowColor={AppColors.darkOrange}
+            darkShadowColor={AppColors.primary}
+            lightShadowColor={AppColors.darkOrange}
             swapShadows // <- change zIndex of each shadow color
             style={ContainerStyles.OrdersContainer2}>
-              <ImageBackground
-              source={require('../../assets/Images/image7.png')}
-              style={{height:hp('15'),width:wp('90'),  borderRadius: 25,
-            }}
-              >
-                <View style={{height:hp('15'),width:wp('90'),backgroundColor: 'rgba(0,0,0,0.5)',  borderRadius: 25,
-}}
-                  >
-                  <Text style={[TextStyles.primaryText2]}>OnGoing Order</Text>
-
-                </View>
-
-              </ImageBackground>
-          {/* <Text style={[TextStyles.smallText,{textAlign:"center",marginRight:wp('3%')}]}>Be A Giving Heart</Text> */}
+          <Text style={[TextStyles.primaryText2]}>OnGoing Order</Text>
 
           </Neomorph>
           </TouchableOpacity>
-          <TouchableOpacity onPress={()=>{
-              navigation.navigate('OngoingOrder')
-            }}>
+          <Neomorph
+            darkShadowColor={AppColors.primary}
+            lightShadowColor={AppColors.darkOrange}
+            swapShadows // <- change zIndex of each shadow color
+            style={[ContainerStyles.smallNeomorphStyle,{marginTop:hp('27%')}]}>
+                        <TouchableOpacity onPress={()=>{
+            navigation.navigate('CompleteOrder')
+          }}>
+
+          <Text style={[TextStyles.primaryText2]}>Confirm Order</Text>
+          </TouchableOpacity>
+
+          </Neomorph>
+          </View>
+          <View style={{marginLeft:wp('5'),marginTop:hp('-8')}}>
           <Neomorph
             darkShadowColor={AppColors.primary}
             lightShadowColor={AppColors.darkOrange}
             swapShadows // <- change zIndex of each shadow color
             style={ContainerStyles.OrdersContainer2}>
-          <Text style={[TextStyles.primaryText]}>Completed Order</Text>
-          {/* <Text style={[TextStyles.smallText,{textAlign:"center",marginRight:wp('3%')}]}>Be A Giving Heart</Text> */}
-
-          </Neomorph>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={()=>{
-              navigation.navigate('OngoingOrder')
+                        <TouchableOpacity onPress={()=>{
+              navigation.navigate('CancelOrder')
             }}>
-          <Neomorph
-            darkShadowColor={AppColors.primary}
-            lightShadowColor={AppColors.darkOrange}
-            swapShadows // <- change zIndex of each shadow color
-            style={ContainerStyles.OrdersContainer2}>
-          <Text style={[TextStyles.primaryText]}>Cancle Order</Text>
-          {/* <Text style={[TextStyles.smallText,{textAlign:"center",marginRight:wp('3%')}]}>Be A Giving Heart</Text> */}
 
-          </Neomorph>
+          <Text style={[TextStyles.primaryText2]}>Cancel Order</Text>
           </TouchableOpacity>
 
-</View>
+          </Neomorph>
+          </View>
+
+
     </SafeAreaView>
   );
 };
